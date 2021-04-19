fef4accb2fcc0dbfe5521734b6f9111bb7dc1422
Jesús Roldán Cadena <jesrolcad@alum.us.es>
2021-04-19 17:39:09 +0200

    TASK FR2- LEVEL C FINISHED
    -Se han listado las tareas públicas.
    -Se muestran los detalles de las tareas públicas.

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -28,12 +27,10 @@ public class Task extends DomainEntity{
 	protected String title;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-	@Future
 	@NotNull
 	protected Date startMoment;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-	@Future
 	@NotNull
 	protected Date endMoment;
 	
