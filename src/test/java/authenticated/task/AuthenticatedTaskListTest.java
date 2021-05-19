3fcc71a939fd09ee73dd7a216cfdb13ce63f7247
raupargor <raupargor@alum.us.es>
2021-05-19 10:21:04 +0200

    Testing Task List Authenticated terminado

--- src/test/java/authenticated/task/AuthenticatedTaskListTest.java
+++ src/test/java/authenticated/task/AuthenticatedTaskListTest.java
@@ -20,15 +20,28 @@ public class AuthenticatedTaskListTest extends AcmePlannerTest {
 		@Order(10)
 		public void positiveAuthenticatedListTask(final int recordIndex, final String title, final String startMoment, final String endMoment,final String description ,final String workload,final String status,final String link) {
 			super.navigateHome();
+			
 			super.signIn("administrator", "administrator");
+			
 			super.clickOnMenu("Authenticated", "List Task");
+			
 			super.checkColumnHasValue(recordIndex, 0, title);
 			super.checkColumnHasValue(recordIndex, 1, startMoment);
 			super.checkColumnHasValue(recordIndex, 2, endMoment);
 			super.checkColumnHasValue(recordIndex, 3, workload);
 
+			super.clickOnListingRecord(recordIndex);
 			
-			super.navigateHome();
+			super.checkInputBoxHasValue("title", title);
+			super.checkInputBoxHasValue("startMoment", startMoment);
+			super.checkInputBoxHasValue("endMoment", endMoment);
+			super.checkInputBoxHasValue("description", description);
+			super.checkInputBoxHasValue("workload", workload);
+			super.checkInputBoxHasValue("status", status);
+			super.checkInputBoxHasValue("link", link);
+
+			super.signOut();
+			
 
 		}
 
