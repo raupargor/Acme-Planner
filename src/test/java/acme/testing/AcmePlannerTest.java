4a6ed53f7f0f143430850cffd84a2f7c9fb54d65
margarcac1 <margarcac1@alum.us.es>
2021-05-19 19:17:30 +0200

    esto es poco importante

--- src/test/java/acme/testing/AcmePlannerTest.java
+++ src/test/java/acme/testing/AcmePlannerTest.java
@@ -25,6 +25,8 @@ public abstract class AcmePlannerTest extends AcmeTest {
 	@BeforeAll
 	public void beforeAll() {
 		super.beforeAll();
+        super.setHeadless(true); //esto se quita
+
 
 		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
 		super.setAutoPausing(false);
--- src/test/java/acme/testing/manager/task/ManagerTaskCreateTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskCreateTest.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import acme.testing.AcmePlannerTest;
 
 public class ManagerTaskCreateTest  extends AcmePlannerTest  {
+	
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/createTask/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void positiveCreateTask(final String title, final String startMoment, final String endMoment,final String status, final String description, final String workload, final String link) {
-
 		super.signIn("manager", "manager");
+
 		super.clickOnMenu("Manager", "Create task");
 		
 		super.fillInputBoxIn("title", title);
