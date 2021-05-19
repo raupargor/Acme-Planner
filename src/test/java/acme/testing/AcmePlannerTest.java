c6918a4900ee22569609f6b66fc45ab7b8fe522a
serfiggom <72518706+Servandofg12@users.noreply.github.com>
2021-05-19 19:01:22 +0200

    Update AcmePlannerTest.java

--- src/test/java/acme/testing/AcmePlannerTest.java
+++ src/test/java/acme/testing/AcmePlannerTest.java
@@ -60,7 +60,7 @@ public abstract class AcmePlannerTest extends AcmeTest {
 		super.checkSimplePath("/master/welcome");
 	}
 
-	protected void signUp(final String username,final String password,final String name,final String surname,final String email,final String phone) {
+	protected void signUp(final String username,final String password,final String name,final String surname,final String email) {
 
 		assert !StringHelper.isBlank(username);
 		assert !StringHelper.isBlank(password);
@@ -79,7 +79,6 @@ public abstract class AcmePlannerTest extends AcmeTest {
 		super.fillInputBoxIn("identity.surname", surname);
 		super.fillInputBoxIn("identity.email", email);
 
-		super.fillInputBoxIn("identity.phone", phone);
 		super.fillInputBoxIn("accept", "true");
 		super.clickOnSubmitButton("Sign Up");
 		super.checkSimplePath("/master/welcome");
