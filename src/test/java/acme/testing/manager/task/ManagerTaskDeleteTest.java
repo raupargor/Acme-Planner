6539810e53bb4f309e36285c093b695c26a6904d
margarcac1 <margarcac1@alum.us.es>
2021-05-20 18:42:42 +0200

    Finalizados los tests de Manager

--- src/test/java/acme/testing/manager/task/ManagerTaskDeleteTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskDeleteTest.java
@@ -1,6 +1,26 @@
 package acme.testing.manager.task;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class ManagerTaskDeleteTest {
+import acme.testing.AcmePlannerTest;
+
+public class ManagerTaskDeleteTest extends AcmePlannerTest {
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/deleteTaskManager/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveDeleteTask(final int recordIndex,final int id) {
+		
+		super.signIn("manager", "manager");
+		
+		super.clickOnMenu("Manager", "List tasks");
+		super.clickOnListingRecord(recordIndex);
+		super.clickOnSubmitButton("Delete Task");
+		super.navigate("/manager/work-plan/show", "id=" + id);
+		super.checkPanicExists();
+	}
 
 }
