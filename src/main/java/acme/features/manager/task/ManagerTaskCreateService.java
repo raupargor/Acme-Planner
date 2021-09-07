4c66cdd36ebc761dd59996324529c69812c9ef43
Jesús Roldán Cadena <72762597+jesrolcad@users.noreply.github.com>
2021-09-07 12:38:57 +0200

    Update ManagerTaskCreateService.java

--- src/main/java/acme/features/manager/task/ManagerTaskCreateService.java
+++ src/main/java/acme/features/manager/task/ManagerTaskCreateService.java
@@ -119,7 +119,8 @@ public class ManagerTaskCreateService implements AbstractCreateService<Manager, 
 		final Double workload = entity.getWorkload();
 		final BigDecimal bd = new BigDecimal(String.valueOf(workload));
 		final BigDecimal fPart = bd.remainder(BigDecimal.ONE);
-		final BigDecimal minutosMax = new BigDecimal(String.valueOf(0.59));
+		final BigDecimal fPartToInt = bd.subtract(bd.setScale(0, RoundingMode.FLOOR)).movePointRight(bd.scale());
+		final BigDecimal minutosMax = new BigDecimal(String.valueOf(59));
 		final Integer workloadInt = workload.intValue();
 		//final Integer workloadDouble = (int) (workload%1.*100);
 		final Boolean workloadCorrecto;
@@ -130,7 +131,7 @@ public class ManagerTaskCreateService implements AbstractCreateService<Manager, 
 	} 
 			else if(workload > 99.59) {
 			errors.state(request, false, "workload","manager.task.error.workloadMax");
-	} else if(fPart.compareTo(minutosMax) == 1) {
+	} else if(fPartToInt.compareTo(minutosMax) == 1) {
 		errors.state(request, false, "workload","manager.task.error.workloadMaxMinutes");
 	} else if(workloadInt > 99) {
 		errors.state(request, false, "workload","manager.task.error.workloadMaxHours");
